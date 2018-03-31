cd0ebf8a436b17ab9a37fff3f489f92cd7823ba3
Alessandro Cifani <alessandro.cifani@gmail.com>
2018-03-31 02:36:21 +0200

    Package for distribution

---
+++ setup.py
@@ -0,0 +1,23 @@
+import io
+from setuptools import setup
+
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+setup(
+    name='boxing',
+    version='0.1.0',
+    url='https://github.com/acifani/boxing',
+    license='BSD',
+    author='Alessandro Cifani',
+    author_email='alessandro.cifani@gmail.com',
+    description='Draw boxes like never before!',
+    long_description=readme,
+    py_modules=['boxing'],
+    classifiers=[
+        'Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python'
+    ])
